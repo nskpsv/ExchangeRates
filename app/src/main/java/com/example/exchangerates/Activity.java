5166b65c7c981744f313ca52ea1487c20edabd68
Sergey Popov <nskpsv@mail.ru>
2021-03-15 23:33:31 +0700

    Рабочая версия. Реализована загрузка данных о курсах валют с сайта ЦБР и отображение в виде списска.

--- app/src/main/java/com/example/exchangerates/Activity.java
+++ app/src/main/java/com/example/exchangerates/Activity.java
@@ -1,7 +1,9 @@
 package com.example.exchangerates;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -35,29 +37,55 @@ public class Activity extends AppCompatActivity implements View {
         list = findViewById(R.id.exchange_rates_list);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public void showRates(Model.ExchangeRates rates) {
 
         ArrayList<HashMap<String, String>> currencyList = new ArrayList<>();
+        HashMap<String, String> currency;
         ListAdapter adapter;
+        double value;
+        double previous;
 
         for (String key : rates.getCurrencyList().keySet()) {
             currencyList.add(rates.getCurrencyList().get(key).getMapCurrency());
         }
 
+        for (int i = 0; i < currencyList.size(); i++) {
+            currency = currencyList.get(i);
+
+            currency.replace("nominal", "за " + currency.get("nominal") + " ед.");
+            currency.replace("currencyCode", currency.get("currencyCode") + "/RUB");
+
+            value = Double.valueOf(currency.get("value"));
+            previous = Double.valueOf(currency.get("previousValue"));
+
+            if (value >= previous) {
+                currency.replace("previousValue", "+" + String.format("%.4f", (value - previous)));
+            } else {
+                currency.replace("previousValue", "" + String.format("%.4f", (value - previous)));
+            }
+
+            currencyList.set(i, currency);
+        }
+
         adapter = new SimpleAdapter(this, currencyList, R.layout.list_item,
-                new String[] {"currencyCode",
+                new String[] {
+                        "currencyCode",
                         "nominal",
                         "currencyName",
                         "value",
                         "previousValue"},
 
-                new int[] {R.id.currency_code,
+                new int[] {
+                        R.id.currency_code,
                         R.id.nominal,
                         R.id.currency_name,
                         R.id.value,
                         R.id.dynamics});
 
         list.setAdapter(adapter);
+
+
     }
 }
